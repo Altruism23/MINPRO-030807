b02116bd3d75ea35f0bdec27b3f36aae7357dc13
ali hafizh <alihafizh970@gmail.com>
2024-05-19 23:47:16 +0700

    feat four

--- apps/web/src/components-home/descrip.tsx
+++ apps/web/src/components-home/descrip.tsx
@@ -6,9 +6,9 @@ export default function Descrip() {
                 <div className="flex">
                     <img src="pict/habi.jpg" alt="gam" className="w-72 mx-10 p-3 rounded-3xl"/>
                     <div className="flex flex-col justify-between ml-10">
-                        <h1 className="font-extrabold text-5xl my-4">
+                        <div className="font-extrabold text-5xl my-4">
                             MUSIC EVENT
-                            <h2 className="font-normal text-xl my-10">
+                            <div className="font-normal text-xl my-10">
                                 <p>
                                     descrip
                                 </p>
@@ -18,8 +18,8 @@ export default function Descrip() {
                                 <p>
                                     dus
                                 </p>
-                            </h2>
-                        </h1>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="absolute bottom-0 right-0 mb-4 mr-36">
@@ -35,94 +35,7 @@ export default function Descrip() {
                 <div className="flex">
                     <img src="pict/habi.jpg" alt="gam" className="w-72 mx-10 p-3 rounded-3xl"/>
                     <div className="flex flex-col justify-between ml-10">
-                        <h1 className="font-extrabold text-5xl my-4">
-                            MUSIC EVENT
-                            <h2 className="font-normal text-xl my-10">
-                                <p>
-                                    descrip
-                                </p>
-                                <p>
-                                    des
-                                </p>
-                                <p>
-                                    dus
-                                </p>
-                            </h2>
-                        </h1>
-                    </div>
-                </div>
-                <div className="absolute bottom-0 right-0 mb-4 mr-36">
-                    <div className="border w-40 h-20 flex justify-center items-center rounded-xl">
-                        <button className="font-extrabold text-xl">
-                            TIKET
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="border my-10 ml-20 mr-20 relative rounded-3xl">
-                <div className="flex">
-                    <img src="pict/habi.jpg" alt="gam" className="w-72 mx-10 p-3 rounded-3xl"/>
-                    <div className="flex flex-col justify-between ml-10">
-                        <h1 className="font-extrabold text-5xl my-4">
-                            MUSIC EVENT
-                            <h2 className="font-normal text-xl my-10">
-                                <p>
-                                    descrip
-                                </p>
-                                <p>
-                                    des
-                                </p>
-                                <p>
-                                    dus
-                                </p>
-                            </h2>
-                        </h1>
-                    </div>
-                </div>
-                <div className="absolute bottom-0 right-0 mb-4 mr-36">
-                    <div className="border w-40 h-20 flex justify-center items-center rounded-xl">
-                        <button className="font-extrabold text-xl">
-                            TIKET
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="border my-10 ml-20 mr-20 relative rounded-3xl">
-                <div className="flex">
-                    <img src="pict/habi.jpg" alt="gam" className="w-72 mx-10 p-3 rounded-3xl"/>
-                    <div className="flex flex-col justify-between ml-10">
-                        <h1 className="font-extrabold text-5xl my-4">
-                            MUSIC EVENT
-                            <h2 className="font-normal text-xl my-10">
-                                <p>
-                                    descrip
-                                </p>
-                                <p>
-                                    des
-                                </p>
-                                <p>
-                                    dus
-                                </p>
-                            </h2>
-                        </h1>
-                    </div>
-                </div>
-                <div className="absolute bottom-0 right-0 mb-4 mr-36">
-                    <div className="border w-40 h-20 flex justify-center items-center rounded-xl">
-                        <button className="font-extrabold text-xl">
-                            TIKET
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="border my-10 ml-20 mr-20 relative rounded-3xl">
-                <div className="flex">
-                    <img src="pict/habi.jpg" alt="gam" className="w-72 mx-10 p-3 rounded-3xl"/>
-                    <div className="flex flex-col justify-between ml-10">
-                        <h1 className="font-extrabold text-5xl my-4">
+                        <div className="font-extrabold text-5xl my-4">
                             MUSIC EVENT
                             <div className="font-normal text-xl my-10">
                                 <p>
@@ -135,7 +48,36 @@ export default function Descrip() {
                                     dus
                                 </p>
                             </div>
-                        </h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute bottom-0 right-0 mb-4 mr-36">
+                    <div className="border w-40 h-20 flex justify-center items-center rounded-xl">
+                        <button className="font-extrabold text-xl">
+                            TIKET
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="border my-10 ml-20 mr-20 relative rounded-3xl">
+                <div className="flex">
+                    <img src="pict/habi.jpg" alt="gam" className="w-72 mx-10 p-3 rounded-3xl"/>
+                    <div className="flex flex-col justify-between ml-10">
+                        <div className="font-extrabold text-5xl my-4">
+                            MUSIC EVENT
+                            <div className="font-normal text-xl my-10">
+                                <p>
+                                    descrip
+                                </p>
+                                <p>
+                                    des
+                                </p>
+                                <p>
+                                    dus
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="absolute bottom-0 right-0 mb-4 mr-36">
